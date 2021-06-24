338e86ccd2e21b3a59f8bfe64afcb8ad3b3f1bdf
sj-ms <jananimani@gmail.com>
2021-06-24 12:10:32 +0530

    more testing

--- ConnectingGit/ViewController.swift
+++ ConnectingGit/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("This is test")
+        print("more of this testing... This is test")
     }
 
 
